f76ad7502b8434dedeada2c40e513fcbc4ce8ca2
Jessica Sousa <jessicacardosops@gmail.com>
2019-08-23 13:06:19 -0300

    adicionar arquivo de instalação

---
+++ install.R
@@ -0,0 +1,17 @@
+# bibliotecas de interface gráfica
+
+install.packages(c("shiny", "shinyalert", "shinythemes"))
+
+# bibliotecas para carregar imagem e ajustar o texto dinamicamente
+# sudo apt install libcairo2-dev
+# sudo apt install libtiff-dev
+
+install.packages(c("imager", "glue"))
+
+
+# biblioteca usada para operações no banco de dados
+# sudo apt install libpq-dev
+
+install.packages(c("DBI", "RPostgres"))
+
+
