f2da53decfbbf1df48df9d20de6ef87fe045cfa2
Jessica Sousa <jessicacardosops@gmail.com>
2019-08-23 23:01:52 -0300

    atualizar arquivo de dataset

--- createDataset.R
+++ createDataset.R
@@ -1,1 +1,29 @@
-f <- list.files('Manga109_Faces',pattern = ".jpg$", recursive = TRUE,full.names=T)
+library(readr)
+
+# connect to the database
+conn <- load_bd_connection()
+
+# create our mangas table with an appropriate 
+# SERIAL data type for the primary key.
+create_table <- "
+CREATE TABLE mangas(
+  id SERIAL PRIMARY KEY,
+  ref text NOT NULL,
+  label integer,
+  busy boolean NOT NULL,
+  marked boolean NOT NULL
+);"
+
+dbExecute(conn, create_table)
+           
+# made up data
+df <- read_csv("ex.csv")
+df[, c("busy", "marked")] <- rep(FALSE, nrow(df))
+
+# write data frame to database
+dbWriteTable(conn, "mangas", df, append=TRUE, row.names=FALSE)
+
+
+
+
+  
