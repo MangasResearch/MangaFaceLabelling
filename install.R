0faf58c16e81bba3ab60188e1925cc16c1feb0d6
(っ◔◡◔)っ ♥ Jeca Manga ♥ <44855238+jecamanga@users.noreply.github.com>
2019-08-25 17:51:07 -0300

    Update install.R

--- install.R
+++ install.R
@@ -3,19 +3,11 @@
 install.packages(c("shiny", "shinyalert", "shinythemes", "markdown"))
 
 # bibliotecas para carregar imagem e ajustar o texto dinamicamente
-# sudo apt install libcairo2-dev
-# sudo apt install libtiff-dev
-# sudo apt install libcurl4-openssl-dev 
-# sudo add-apt-repository -y ppa:cran/imagemagick
-# sudo apt-get update
-# sudo apt-get install -y libmagick++-dev
 
-install.packages(c("magick", "rsvg", "glue"))
+install.packages(c("magick", "tibble", "glue"))
 
 
 # biblioteca usada para operações no banco de dados
-# sudo apt install libpq-dev
-
 install.packages(c("DBI", "RPostgres"))
 
 
